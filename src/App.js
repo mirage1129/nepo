b888f2b1aeb603862f53130c10741f522b6a6363
Meraj Qazi <meraj.a.qazi@gmail.com>
2024-09-26 13:32:08 +0100

    added tailwind

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and saaaaave to reload.
         </p>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
